82285d397f652f75a0e69800f07261d341a7170b
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-16 13:30:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const router = express.Router();
-const connectDB = require('./models/connectDB')
-const router= require('./routers/router');
+const connectDB = require("./models/connectDB");
+const router = require("./routers/router");
 
 require("dotenv").config();
 
-const port = 5002;
+// production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
+});
+
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 connectDB();
-
-app.listen(port,()=>{
-    console.log(`I am listening to ${port}`);
-});

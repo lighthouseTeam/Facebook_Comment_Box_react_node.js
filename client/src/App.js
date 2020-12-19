55909d73edb9d25fa463ff1d58dd02e5d63d38eb
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-19 04:03:37 +0300

    fix: delet .eslintcache

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,34 @@
-import React, {useState, useEffect} from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import axios from "axios";
 import CommentList from "./CommentList";
 import CommentForm from "./CommentForm";
 
-
-
-
-const  App = () => {
+const App = () => {
   const [data, setData] = useState([]);
-   
-
 
   const fetchData = async () => {
-   
-    const response = await axios
-        .get("/api")
-        .then((res) => setData(res.data))
-        .catch((error) => console.log(error));
-
+    await axios
+      .get("/api")
+      .then((res) => setData(res.data))
+      .catch((error) => console.log(error));
   };
- 
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchData();
   }, []);
 
   return (
-
     <div>
-    <div>
-      <h2>Comments:</h2>
-      <CommentList data={data} />
+      <div>
+        <h2>Comments:</h2>
+        <CommentList data={data} />
+      </div>
+      <div>
+        <CommentForm />
+      </div>
     </div>
-    <div>
-      <CommentForm/>
-
-    </div>
-  </div>
   );
-}
+};
 
 export default App;

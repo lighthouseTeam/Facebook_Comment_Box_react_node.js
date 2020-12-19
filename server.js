41fa3cc5e5bdc936c0dbbceb3ab964adeb209a12
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-19 03:17:49 +0300

    fix: .gitignore

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const app = express();
 // const router = express.Router();
 const connectDB = require("./models/connectDB");
 const router = require("./routers/router");
-<<<<<<< HEAD
-// const cors = require("cors");
-=======
-const cors = require('cors');
->>>>>>> 83b09e257728d9acacb1ce4d7336018384c8fd65
+//const cors = require('cors');
 
 require("dotenv").config();
 
@@ -23,12 +19,8 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
-<<<<<<< HEAD
 // app.use(cors());
-=======
 
-app.use(cors());
->>>>>>> 83b09e257728d9acacb1ce4d7336018384c8fd65
 app.use(express.json());
 app.use("/", router);
 
